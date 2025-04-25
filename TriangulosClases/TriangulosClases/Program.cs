51d580dc0d1e20313033f3381362858a77a3d3d1
DM8307 <1128307@est.intec.edu.do>
2025-04-24 23:21:18 -0400

    Add files via upload
    Triángulos con clases

---
+++ TriangulosClases/TriangulosClases/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TriangulosClases;
+
+bool condicion = true;
+string eleccion;
+while (condicion)
+{
+	Console.Write($"\n1. - Que tipo de triangulo es? \n2. - Salir \n:");
+	eleccion = Console.ReadLine();
+    Console.Clear();
+	switch (eleccion)
+	{
+		case "1":
+            Console.WriteLine("Ingrese el valor del lado 1 del triángulo:");
+            double lado1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor del lado 2 del triángulo:");
+            double lado2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor del lado 3 del triángulo:");
+            double lado3 = Convert.ToDouble(Console.ReadLine());
+
+            Triangulos triangulo = new Triangulos(lado1, lado2, lado3);
+
+            triangulo.MostrarLados();
+
+            if (triangulo.EsTriangulo())
+            {
+                Console.WriteLine("Es un triángulo válido.");
+                Console.WriteLine($"Tipo de triángulo: {triangulo.TipoTriangulo()}");
+            }
+            else
+            {
+                Console.WriteLine("Los lados ingresados no forman un triángulo.");
+            }
+            break;
+		case "2":
+			condicion = false;
+			break;
+		default:
+			Console.WriteLine("Debe seleccionar una opcion. ");
+			break;
+	}
+}
---
+++ TriangulosClases/TriangulosClases/Triangulos.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangulosClases
+{
+    public class Triangulos
+    {
+        private double lado1;
+        private double lado2;
+        private double lado3;
+
+        // Constructor para almacenar los lados y luego verificar
+        public Triangulos(double l1, double l2, double l3)
+        {
+            lado1 = l1;
+            lado2 = l2;
+            lado3 = l3;
+        }
+
+        // Método para mostrar los lados
+        public void MostrarLados()
+        {
+            Console.WriteLine($"Lados del triángulo: {lado1}, {lado2}, {lado3}");
+        }
+
+        // Método para verificar si es un triángulo válido
+        public bool EsTriangulo()
+        {
+            return (lado1 + lado2 > lado3) && (lado1 + lado3 > lado2) && (lado2 + lado3 > lado1);
+        }
+
+        // Método para determinar el tipo de triángulo
+        public string TipoTriangulo()
+        {
+            if (!EsTriangulo())
+            {
+                return "No es un triángulo válido";
+            }
+
+            if (lado1 == lado2 && lado2 == lado3)
+            {
+                return "Triángulo Equilátero";
+            }
+            else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+            {
+                return "Triángulo Isósceles";
+            }
+            else
+            {
+                return "Triángulo Escaleno";
+            }
+        }
+    }
+}
